47b9064d5dbdf0977624bd141eebc1c4796a4173
Robert McCreary Mannino <fooyinfighter@gmail.com>
2025-02-08 09:52:25 -0800

    add list of only chengyu

---
+++ data/node-extract-chengyu.js
@@ -0,0 +1,33 @@
+// node.js
+// open file raw-text.txt
+
+var fs = require('fs');
+var path = require('path');
+
+// get the text
+var text = fs.readFileSync(path.join(__dirname, 'raw-text.txt'), 'utf8');
+
+// select only lines that contain '成語：'
+var lines = text.split('\n').filter(function(line) {
+  return line.indexOf('成語：') > -1;
+});
+
+// extract the chengyu
+var chengyu = lines.map(function(line) {
+  return line.split('：')[1].trim();
+});
+
+// output the chengyu
+// save to chengyu.txt
+fs.writeFileSync(path.join(__dirname, 'chengyu.txt'), chengyu.join('\n'), 'utf8');
+
+// create html page with each chengyu in a p tag
+var html = '<!doctype html><html><head><title>Chengyu</title></head><body>';
+chengyu.forEach(function(c) {
+  html += '<p>' + c + '</p>';
+});
+html += '</body></html>';
+// save to chengyu-list.html
+fs.writeFileSync(path.join(__dirname, 'chengyu-list-raw.html'), html, 'utf8');
+
+console.log('chengyu.txt saved');
